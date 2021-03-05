0f746c073fe479d1b01b72c5531f9315adaca660
Nate J <jamis.walsh@gmail.com>
2021-03-05 00:12:52 -0800

    committing what I have so far...

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-
+const render = require("./lib/htmlRender");
 const inquirer = require("inquirer");
 const path = require("path")
 const fs = require("fs")
 
+const distDIR = path.resolve(__dirname, "dist");
+const distPath = path.join(distDIR, "myteam.html");
 
-
+let HTML = "";
+const teamList = [];
 
 const mgrQuestions = [
     {
@@ -18,7 +21,7 @@ const mgrQuestions = [
     {
         type: "input",
         message: "Employee ID?",
-        name: "empId"
+        name: "id"
     },
     {
         type: "input",
@@ -28,9 +31,9 @@ const mgrQuestions = [
     {
         type: "input",
         message: "Office number?",
-        name: "ofcNumber"
+        name: "officeNumber"
     }
-]
+];
 
 const engQuestions = [
     {
@@ -41,7 +44,7 @@ const engQuestions = [
     {
         type: "input",
         message: "Employee ID?",
-        name: "empId"
+        name: "id"
     },
     {
         type: "input",
@@ -53,7 +56,7 @@ const engQuestions = [
         message: "Github username?",
         name: "github"
     }
-]
+];
 
 const internQuestions = [
     {
@@ -64,7 +67,7 @@ const internQuestions = [
     {
         type: "input",
         message: "Employee ID?",
-        name: "empId"
+        name: "id"
     },
     {
         type: "input",
@@ -76,4 +79,66 @@ const internQuestions = [
         message: "Intern's school?",
         name: "school"
     }
-]
+];
+
+const empInfo = () => {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "Employee's role?",
+            name: "role",
+            choices: ["Manager", "Engineer", "Intern"]
+        }
+    ]).then(selection => {
+        if (selection.role === "Manager") {
+            mgrInfo();
+        } else if (selection.role === "Engineer") {
+            engInfo();
+        } else {
+            internInfo();
+        };
+        function mgrInfo() {
+            inquirer.prompt(mgrQuestions)
+            .then(selection => {
+                const employee = new Manager(selection.name, selection.role, selection.id, selection.email, selection.officeNumber);
+                teamList.push(employee);
+                addEmployee();
+            })
+        };
+        function engInfo() {
+            inquirer.prompt(engQuestions)
+            .then(selection => {
+                const employee = new Engineer(selection.name, selection.role, selection.id, selection.email, selection.github);
+                teamList.push(employee);
+                addEmployee();
+            })
+        };
+        function internInfo() {
+            inquirer.prompt(internQuestions)
+            .then(selection => {
+                const employee = new Intern(selection.name, selection.role, selection.id, selection.email, selection.school);
+                teamList.push(employee);
+                addEmployee();
+            })
+        };
+    })
+};
+const addEmployee = () => {
+    inquirer.prompt([
+        {
+            type: "confirm",
+            message: "Add another employee?",
+            name: "addEmployee"
+        }
+    ]).then(response => {
+        if (response.addEmployee === true) {
+            empInfo();
+        } else {
+            HTML += render(teamList);
+            fs.writeFile(distPath, HTML, err => console.log(err))
+            console.log("Team list has been created!")
+            return;
+        }
+    })
+};
+empInfo();
